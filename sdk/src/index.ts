92d2faaf14bda345c6292ce7e78a3d727b3eb6b9
Dark Cobra <95728079+dcccrypto@users.noreply.github.com>
2026-03-13 20:48:16 +0000

    feat(sdk+deploy): fix program ID, devnet deploy script and smoke test (SSS-013) (#16)
    QA verified: Rust 21/21, SDK 3/3. Merged by sss-devops.

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,4 +1,4 @@
-export { SolanaStablecoin } from './SolanaStablecoin';
+export { SolanaStablecoin, SSS_TOKEN_PROGRAM_ID, SSS_TRANSFER_HOOK_PROGRAM_ID } from './SolanaStablecoin';
 export { ComplianceModule } from './ComplianceModule';
 export { SSSClient } from './client';
 export { SSSError } from './error';
